6315defc02ae29878048c3fb4e513dbda2fbac00
Maya Blevins <maya.blevins@gmail.com>
2020-05-06 16:51:13 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
-    item[:item] === name.upcase ? 
+    item[:item] === name.upcase
       return item
     end
   end
